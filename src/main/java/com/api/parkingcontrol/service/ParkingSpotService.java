d96e7b71a6827eb5be4d84e09ff77e606ab68d5b
riBergamo <rrianebergamo@gmail.com>
2023-02-27 16:10:04 -0300

    creating parking spot service

--- src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -1,2 +1,18 @@
-package com.api.parkingcontrol.service;public class ParkingSpotService {
+package com.api.parkingcontrol.service;
+
+import com.api.parkingcontrol.repository.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+//para melhorar: criar interface para o service para esta classe implementar ela pq se caso precisar trocar a implementação nao vai precisar fazer refatoração grande no controller
+@Service
+public class ParkingSpotService {
+
+    //final:
+    final ParkingSpotRepository parkingSpotRepository; //controller assiona service, service assiona repository
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {//criar injeção de dependencias: @autowared ou (melhor) criar construtor
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+
 }
