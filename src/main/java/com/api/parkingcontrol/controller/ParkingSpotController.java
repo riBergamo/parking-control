d33624abc95626e5cb031292b69cd56dee9d8825
riBergamo <rrianebergamo@gmail.com>
2023-03-02 10:03:51 -0300

    creating parking spot controller

--- src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
@@ -1,2 +1,20 @@
-package com.api.parkingcontrol.controller;public class ParkingSpotController {
+package com.api.parkingcontrol.controller;
+
+import com.api.parkingcontrol.service.ParkingSpotService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+
+
 }
