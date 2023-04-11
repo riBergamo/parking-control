c923c1a0cccd6dc62675f4eb9b80be1324ed9704
riBergamo <rrianebergamo@gmail.com>
2023-04-11 09:11:44 -0300

    Authentication in memory

--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityOld.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityOld.java
@@ -1,18 +1,41 @@
 package com.api.parkingcontrol.configs.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@Configuration//@enablewebsecurity: desliga as configs do spring security
 public class WebSecurityOld extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {//nao esta especificado nenhuma requisição então vai permitar o acesso sem autenticação
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("riane")
+                .password(passwordEncoder().encode("1234"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
+// para post e delete tem que fazer config por segurança, por causa do crsf hablitado:
+// recomendado desbilitar apenas quando nao é um site aberto para navegadores apenas pra clientes etc, e se for utilizar ele veja como na documentação
